5ff3c2b46f148b38d3f80b2ad5c87cb62e100967
mayavathi <mayachandre27@gmail.com>
2021-06-04 11:47:35 -0400

    UC1_Take_three_inputs

--- arthmatic.sh
+++ arthmatic.sh
@@ -1,3 +1,8 @@
 #!bin/bash
 
 echo " Arithmetic Computation and Sorting"
+
+read -p "Enter first input :" firstInput
+read -p "Enter second input :" secondInput
+read -p "Enter third input :" thirdInput
+
